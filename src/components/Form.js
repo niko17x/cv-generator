7804f6fd3ade9a9a0c23a423e733e81e377f623f
Niko Shin <nshin17@gmail.com>
2023-02-28 13:20:17 -0800

    Added function to create additional workInfo component and workInfo object if user clicks on add button. Allows user to enter more work info if necessary.

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,43 +6,25 @@ import CvDoc from "./CvDoc";
 
 function Form() {
   const [userInfo, setUserInfo] = React.useState(userData);
+  const [newWorkInfo, setNewWorkInfo] = React.useState(1); // Default with 1 workInfo Component.
 
-  function handleChange(e) {
-    const { name, value } = e.target;
-    // const [firstPart, secondPart] = name.split(".");
-    // setUserInfo((prevInfo) => ({
-    //   generalInfo: {
-    //     ...prevInfo.generalInfo,
-    //     [name]: value,
-    //   },
-    //   workInfo: {
-    //     ...prevInfo.workInfo,
-    //     work1: {
-    //       ...prevInfo.workInfo.work1,
-    //       [name]: value,
-    //     },
-    //   },
-    // }));
-    if (name in userInfo.generalInfo) {
-      setUserInfo((prevInfo) => ({
-        ...prevInfo,
-        generalInfo: {
-          ...prevInfo.generalInfo,
-          [name]: value,
-        },
-      }));
-    } else {
-      setUserInfo((prevInfo) => ({
-        ...prevInfo,
-        workInfo: {
-          ...prevInfo.workInfo,
-          work1: {
-            ...prevInfo.workInfo.work1,
+  function handleChange(event, parent, child) {
+    const { name, value } = event.target;
+    setUserInfo((prevInfo) => ({
+      ...prevInfo,
+      [parent]: {
+        ...prevInfo[parent],
+        ...(child && {
+          [child]: {
+            ...prevInfo[parent][child],
             [name]: value,
           },
-        },
-      }));
-    }
+        }),
+        ...(child === undefined && {
+          [name]: value,
+        }),
+      },
+    }));
   }
 
   function handleSubmit(e) {
@@ -53,26 +35,78 @@ function Form() {
   }
 
   // Adds another object to the existing userData database :
-  function addAdditionalWork() {}
+  function addAdditionalWorkObj() {
+    for (let i = 1; i < newWorkInfo + 1; i++) {
+      setUserInfo((prevInfo) => ({
+        ...prevInfo,
+        workInfo: {
+          ...prevInfo.workInfo,
+          [`work${i + 1}`]: {
+            position: "",
+            company: "",
+            location: "",
+            achievements: "",
+            startDate: "",
+            endDate: "",
+          },
+        },
+      }));
+    }
+  }
 
-  function foo(e) {
-    console.log(e.target.value);
+  // Need to generate CvDoc 'work2' if it is truthy :
+  // Go through key key in workInfo object and if 'work2' exists, then generate 'work2' data in CvDocs.
+  function foo() {
+    let result = [];
+    const getWorkObj = userInfo.workInfo;
+    if (getWorkObj.hasOwnProperty("work2")) {
+      return <CvDoc name={getWorkObj.work2.company} />;
+    }
+  }
+
+  // Creates a new 'workInfo' component with inputs based on number of 'add button clicks' :
+  function addAdditionalWorkForm() {
+    let result = [];
+    for (let i = 1; i <= newWorkInfo; i++) {
+      result.push(
+        <div>
+          <WorkInfo
+            key={i}
+            value={userInfo.workInfo + `${i}`}
+            onChange={(event) => handleChange(event, "workInfo", `work${i}`)}
+          />
+        </div>
+      );
+    }
+    return result;
+  }
+
+  function handleAddButtonClick() {
+    if (newWorkInfo > 4) return;
+    setNewWorkInfo((prevInfo) => prevInfo + 1);
+    addAdditionalWorkObj();
   }
 
   return (
     <div className="form--main">
       <form onSubmit={handleSubmit}>
         <div className="form--generalInfo_container">
-          <GeneralInfo value={userInfo} onChange={handleChange} />
+          <GeneralInfo
+            value={userInfo}
+            onChange={(event) => handleChange(event, "generalInfo")}
+          />
         </div>
-        <div>
-          <WorkInfo value={userInfo || ""} onChange={handleChange} foo={foo} />
-        </div>
+        {/* <div>
+          <WorkInfo
+            value={userInfo.workInfo}
+            onChange={(event) => handleChange(event, "workInfo", "work1")}
+          />
+        </div> */}
+        {newWorkInfo && addAdditionalWorkForm()}
         <button type="submit">Submit</button>
       </form>
       <CvDoc name={userInfo} />
-      {/* <CvDoc name={userInfo.generalInfo.name} /> */}
-      <button className="add" onClick={addAdditionalWork}>
+      <button className="add" onClick={handleAddButtonClick}>
         Add
       </button>
     </div>
