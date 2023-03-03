af9837db088b274798fd9e6d61dc0903fca0a649
Niko Shin <nshin17@gmail.com>
2023-03-02 20:06:00 -0800

    Seperated to its own component for easier configuration later.

---
+++ src/components/General.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Education from "./Education";
+import Professional from "./Professional";
+
+function General({ name }) {
+  return (
+    <div className="general--container">
+      <div className="general--left">
+        <img src="../profile-img-outline.svg" alt="" />
+      </div>
+
+      <div className="general--right">
+        <div className="profile_name">
+          <h2>
+            <span className="first_name">{name.generalInfo.name}</span>
+          </h2>
+        </div>
+        <div className="profile_position">
+          <h3>{name.generalInfo.currentPosition}</h3>
+        </div>
+        <div className="profile_about_me">
+          <p>{name.generalInfo.aboutMe}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default General;
