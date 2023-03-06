45eb3f4adb4e8da2a4d299611d2274a989b6380d
Niko Shin <nshin17@gmail.com>
2023-03-06 07:42:28 -0800

    Edited image upload.

---
+++ src/components/ProfileImg.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function ProfileImg(props) {
+  return (
+    <div className="profile-image">
+      <label for="img">Upload Image</label>
+      <input
+        type="file"
+        id="img"
+        name="img"
+        accept="image/*"
+        onChange={props.handleFileChange}
+      />
+      {/* {imageSrc && <DisplayImage imageUrl={imageSrc} />} */}
+    </div>
+  );
+}
+
+export default ProfileImg;
